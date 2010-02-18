ee4d197f8b43454d2c6a177ac068c237d762339f
nobody <nobody@7f1ebc58-12aa-11df-83e8-2f775389245c>
2010-02-18 08:17:17 +0000

    fix email address

--- WidgetRunnerAppDelegate.m
+++ WidgetRunnerAppDelegate.m
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010 Samuel R Madden (madden@csai.mit.edu)
+/* Copyright (c) 2010 Samuel R Madden (madden@csail.mit.edu)
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
